c8541066e82b7cb4c02c4cfb89e605a1779d60e4
Mateusz <matikg@interia.eu>
2024-04-18 22:37:22 +0200

    Initial Commit

---
+++ ApplePie/ApplePieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplePieApp.swift
+//  ApplePie
+//
+//  Created by Mateusz Grudzień on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ApplePieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
